3ecd50b090652e6c1388185a6d6d563fdc9bb342
Kjell Breiland <kjell.breiland@gmail.com>
2018-09-25 19:01:37 -0700

    capturing keyboard imput in tests is... challenging

---
+++ keyboard_test.go
@@ -0,0 +1,24 @@
+// Package keyboard emulates key presses in windows using user32.dll
+
+package keyboard
+
+import "testing"
+
+func TestKeyPress_Press(t *testing.T) {
+	p := stringToPress("K")
+
+	err := p.Press()
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Press Delete to clear the a from whatever just took the input
+
+	// var s = 6000
+	// p = KeyPress{
+	// 	Key:   0x08,
+	// 	Sleep: &s,
+	// }
+
+	// p.Press()
+}
